4017c9efd3669e1e4108a7d50ed176e1798dcfa2
Harsh Kumar <hadron43@yahoo.com>
2021-03-23 00:30:25 +0530

    pages: home page TopCard completed

--- src/pages/Home/HomeCard.jsx
+++ src/pages/Home/HomeCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Button} from 'reactstrap';
+import {Container, Row, Col, Button} from 'reactstrap';
 import {Link} from 'react-router-dom';
 
 function UpdatesCard({heading, updates, buttonText, buttonLink}) {
@@ -14,6 +14,8 @@ function UpdatesCard({heading, updates, buttonText, buttonLink}) {
                 );
             })}
 
+            <br />
+
             <div className="bottom clearfix w-100">
                 <Link to={buttonLink}>
                     <Button color="info" className="mt-2 float-right bottom-button bg-color-main-ui">{buttonText}</Button>
@@ -23,4 +25,46 @@ function UpdatesCard({heading, updates, buttonText, buttonLink}) {
     );
 }
 
-export default UpdatesCard;
+function rankToColor(rank) {
+    var color = "bg-white";
+    if(rank === 1)
+        color = "bg-warning";
+    else if(rank === 2)
+        color = "bg-color-silver";
+    else if(rank === 3)
+        color = "bg-color-bronze";
+    return color;
+}
+
+function TopCard({heading, toppers, buttonText, buttonLink}) {
+    return (
+        <Container fluid className="shadow rounded-lg bg-color-lightest-grey p-4 pb-5 h-100">
+            
+            <h4 className="text-color-main font-weight-bold mb-4">{heading}</h4>
+            
+            {toppers.map((update) => {
+                return (
+                    <Row className="mb-2">
+                        <Col xs="1" className="d-flex">
+                            <p className="d-flex m-auto">{update.rank}</p>
+                        </Col>
+                        <Col xs="8" className="d-flex">
+                        <p className="d-flex mt-auto mb-auto">{update.name}</p>
+                        </Col>
+                        <Col xs="2" className={`d-flex rounded p-2 ${rankToColor(update.rank)}`}>
+                            <p className="d-flex m-auto">{update.points}</p>
+                        </Col>
+                    </Row>
+                );
+            })}
+            <br />
+            <div className="bottom clearfix w-100">
+                <Link to={buttonLink}>
+                    <Button color="info" className="mt-2 float-right bottom-button bg-color-main-ui">{buttonText}</Button>
+                </Link>
+            </div>
+        </Container>
+    );
+}
+
+export {UpdatesCard, TopCard};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,55 +1,9 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+
 import Banner from './Banner';
-import UpdatesCard from './HomeCard';
-
-function Pubs() {
-    return (
-        <UpdatesCard
-            heading="Recent Publications"
-            updates={[
-                "Student X published Y papaer in Z conference. Congratulations to the whole team!",
-                "Student X published Y papaer in Z conference. Congratulations to the whole team!",
-                "Student X published Y papaer in Z conference. Congratulations to the whole team!",
-                "Student X published Y papaer in Z conference. Congratulations to the whole team!"
-            ]}
-            buttonText="More Updates"
-            buttonLink=""
-        />
-    )
-}
-
-function Staff() {
-    return (
-        <UpdatesCard
-            heading="Staff Achievements"
-            updates={[
-                "Staff X achieved Y papaer in Z. Congratulations to the whole team!",
-                "Staff X achieved Y papaer in Z. Congratulations to the whole team!",
-                "Staff X achieved Y papaer in Z. Congratulations to the whole team!",
-                "Staff X achieved Y papaer in Z. Congratulations to the whole team!",
-            ]}
-            buttonText="More Updates"
-            buttonLink=""
-        />
-    )
-}
-
-function Students() {
-    return (
-        <UpdatesCard
-            heading="Student Achievements"
-            updates={[
-                "Student X achieved Y papaer in Z. Congratulations to the whole team!",
-                "Student X achieved Y papaer in Z. Congratulations to the whole team!",
-                "Student X achieved Y papaer in Z. Congratulations to the whole team!",
-                "Student X achieved Y papaer in Z. Congratulations to the whole team!",
-            ]}
-            buttonText="More Updates"
-            buttonLink=""
-        />
-    )
-}
+import { UpdatesCard, TopCard } from './HomeCard';
+import data from './updates'
 
 function Home() {
     return (
@@ -58,14 +12,26 @@ function Home() {
 
         <Container fluid className="mt-3 mb-3">
             <Row equal className="p-1 p-sm-2 p-lg-3 p-xl-5">
-                <Col size="12" sm="6" md="4" xxl="3" className="mb-4">
-                    <div className="col-12 h-100"><Pubs /></div>
+                <Col size="12" sm="6" lg="4" xxl="3" className="mb-4">
+                    <div className="col-12 h-100"><UpdatesCard {...data.publications}/></div>
                 </Col>
-                <Col size="12" sm="6" md="4" xxl="3" className="mb-4">
-                    <div className="col-12 h-100"><Staff /></div>
+                <Col size="12" sm="6" lg="4" xxl="3" className="mb-4">
+                    <div className="col-12 h-100"><UpdatesCard {...data.staff}/></div>
                 </Col>
-                <Col size="12" sm="6" md="4" xxl="3" className="mb-4">
-                    <div className="col-12 h-100"><Students /></div>
+                <Col size="12" sm="6" lg="4" xxl="3" className="mb-4">
+                    <div className="col-12 h-100"><UpdatesCard {...data.students}/></div>
+                </Col>
+            </Row>
+
+            <Row equal className="p-1 p-sm-2 p-lg-3 p-xl-5">
+                <Col size="12" sm="6" lg="4" xxl="3" className="mb-4">
+                    <div className="col-12 h-100"><TopCard {...data.active_departments}/></div>
+                </Col>
+                <Col size="12" sm="6" lg="4" xxl="3" className="mb-4">
+                    <div className="col-12 h-100"><TopCard {...data.active_students}/></div>
+                </Col>
+                <Col size="12" sm="6" lg="4" xxl="3" className="mb-4">
+                    <div className="col-12 h-100"><TopCard {...data.active_staff}/></div>
                 </Col>
             </Row>
         </Container>
