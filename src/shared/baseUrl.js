bdff6ead9c6cf5a482d76c27597642681a5948ae
Harsh Kumar <hadron43@yahoo.com>
2022-05-21 06:36:16 +0530

    update baseUrl

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,3 +1,3 @@
-export const baseUrl_ = 'http://achieve.osa.iiitd.edu.in';
+export const baseUrl_ = 'http://achieve.fh.iiitd.edu.in';
 // export const baseUrl_ = 'https://achievements-midas.herokuapp.com';
 export const baseUrl = baseUrl_ + '/';
